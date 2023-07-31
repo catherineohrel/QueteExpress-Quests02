8646c02675d3c669dedf6f69d7cf7707c91018e6
catherineohrel <catherine_ohrel@hotmail.com>
2023-07-31 17:00:25 +0200

    quete-05

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUser);
--- userHandlers.js
+++ userHandlers.js
@@ -65,7 +65,25 @@ const updateUser = (req, res) => {
   });
 };
 
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database.query("DELETE FROM users WHERE id = ?", [id])
+  .then(([result]) => {
+    if (result.affectedRows === 0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.sendStatus(204);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error deletting the user");
+  });
+};
+
 module.exports = {
+  deleteUser,
   getUsers,
   getUserById,
   postUser,
