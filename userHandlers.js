b153b9732648b70c0c92b17d464d59911e534c53
catherineohrel <catherine_ohrel@hotmail.com>
2023-08-21 16:22:10 +0200

    quete-08

--- userHandlers.js
+++ userHandlers.js
@@ -94,6 +94,26 @@ const deleteUser = (req, res) => {
   });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 
 
 module.exports = {
@@ -102,4 +122,5 @@ module.exports = {
   getUserById,
   postUser,
   updateUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
